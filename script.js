ea41082157f4381f392605be7b189e619979768b
Yaqoob Farhood <yaqoobnoorzai1382@gmail.com>
2024-09-07 07:59:47 +0430

    new practice in JavaScript

---
+++ script.js
@@ -0,0 +1,344 @@
+// My first code 
+
+// console.log('this is my fist code')
+
+// let fruit = 'apple'
+
+// (3)if(fruit==="apple")console.log()
+
+
+// (4)JAVA Script is :-
+/* 1: High level language: means simple to use easy to understand and no complexity.
+2: Object Orented: means store data in objects.
+3: multi paragidm: means flexible and versatile.
+Logic: Feaching data, click events etc.
+*/
+
+// (5)Linking a Javascript File 
+/* 1: Link our Javascript in HTML file.
+
+Values & Variables 
+let fruit = 'apple';
+
+console.log("Micaheal");
+console.log(23);
+console.log( 40 + 50 + 10);
+
+let firstname = 'Ali';
+console.log(firstname);
+console.log(firstname);
+console.log(firstname);
+*/
+
+
+// (6)Data Types 
+/* PRIMITIVE & OBJECT
+1:- Primitive datat type: there are 7 primitive data types
+1 Number = disimals
+2 String = Charactera EX: "ALi"
+3 Boolean = to take disiton true and false 
+4 Undefined = means empty value take a varible that not yet defined without asineing a value
+5 Null = its like undefined but its use for difrent circle imstanses
+6 Symbol = define a value that its uniq and not changable its not very useful for us
+7 BigInt = for the intejers that are too larg to be represented by number type 
+but there is another feuter in JavaScript = automaticly determ is the data type value when store in a value, value have a type not varible.
+
+const appleaIsfruit = true;
+console.log(appleaIsfruit);
+console.log(typeof true);
+console.log(typeof appleaIsfruit);
+console.log(typeof 23432);
+console.log(typeof 'aside');
+
+let _undefined;
+console.log(_undefined);
+console.log(typeof _undefined);
+
+_undefined = 25;
+console.log(_undefined);
+console.log(typeof _undefined);
+console.log(typeof null)
+*/
+
+// (7) Let, Const & Var Keywords
+/* 
+let Keyword = to declear varibles that can change later. 
+const keyword = is not changabl, if repet (error).
+Var keyword = copmletly avoid it never use it .
+let price = 123;
+price = 333
+console.log(price)
+
+const price = 123;
+price = 333
+console.log(price)
+*/
+
+// (8) Basic Operators
+// operators = basicly allows us to combine multiple values
+// with difrent catagores like mathimaticle operators asignment operators and MutationRecord....
+/*
+const curentYear = 2024;
+const age1 = curentYear - 1996;
+const age2 = curentYear - 2005;
+console.log(age1 * 2)
+console.log(age2 / 3)
+
+const firstname = 'yaqoob';
+const surname = 'Farhood';
+console.log(firstname + ' ' + surname);
+
+// Assignment opreator
+let fullName = firstname + surname;
+console.log(fullName);
+
+let counter = 0;
+counter += 10;
+console.log(counter);
+
+counter *= 5;
+console.log(counter);
+
+counter /= 2;
+console.log(counter);
+
+counter++;
+console.log(counter);
+
+counter--;
+counter--;
+counter--;
+console.log(counter);
+
+// comparison operators
+console.log(age1 < age2);
+console.log(age1 > age2);
+console.log(age1 <= age2);
+*/
+
+// 9.Operator precidence
+/*
+const curentYear = 2022;
+const age1 = curentYear - 1996;
+const age2 = curentYear - 2005;
+
+console.log(curentYear - 1996 > curentYear - 2005);
+console.log(100 - 30 - 10); 
+
+let a, b;
+a = b = 100 - 30 - 10;
+console.log(a, b); 
+console.log(age1, age2);
+const average = (age1 + age2) / 2;
+console.log(average); 
+*/
+
+// 10. String & Template Litral
+/*
+const brandName = 'Toyota';
+const modelName = 'corola';
+const year = 2015;
+
+const myCar = 'My car is' + ' ' + brandName + ' ' + modelName + ' I bougth it in ' + year;
+console.log(myCar); 
+
+const myCar2 = `My car is ${brandName} ${modelName} I bougth it in ${year} `;
+console.log(myCar2);
+*/
+
+// 11. If else Statement
+/*
+const personName = prompt ("What is your nam?");
+const personAge = prompt ("How Old are you?");
+const isLegal = personAge >= 7 ;
+if (isLegal){
+    console.log(`${personName} Jan You can get fast!`);
+} else{
+    const yearsLeft = 7 - personAge;
+    console.log(`${personName} Jan you can get fast ${yearsLeft} years later!`)
+}
+*/
+// Calculator with if statements 
+/*
+const num1 = prompt ("Enter your first number");
+const opreator = prompt ("Enters Your Operator");
+const num2 = prompt ("Enter your second number");
+
+
+let result;
+
+ if ( opreator == '+') {
+    result = num1 + num2;
+ } else if( opreator == '-') {
+    result = num1 - num2;
+ } else if( opreator == '*') {
+    result = num1 * num2;
+ } else {
+    result = num1 / num2;
+ }
+
+console.log(`The result of ${num1}${opreator}${num2} is equal to = ${result}`);
+*/
+
+// 12. Type Convertion & Covertion
+// // 1= Type Convertion: is manivalited from one type to another type.  
+// const inputAge = '26';
+// console.log(inputAge + 3)
+
+// // Type Covertion: JavaScript automaticly convert types behind the seen for us.
+// console.log('I was born in' + 1999);
+// console.log('I was born in' + '1999');
+
+
+// 13. Truthy & falsy Values
+// falsy Values: 0, '' , undefined, null, nan  withouth this all other values are thrue.
+/*
+console.log(Boolean(0))
+console.log(Boolean(undefined));
+console.log(Boolean('Yaqoob'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+ 
+
+const candy = 0;
+if(555){
+   console.log("Do not eat so much candy");
+} else{
+   console.log("You do not have candy");
+}
+
+const candyes = 0;
+if(candyes){
+   console.log("Do not eat so much candy");
+} else{
+   console.log("You do not have candy");
+}
+*/
+
+
+// 14. Equality Operators 
+
+// const age = 18;
+// if (age === 18) console.log('You are of legal age (===)');
+// if (age == 18) console.log('You are of legal age (== )');
+
+// const ages = '18';
+// if (ages === 18) console.log('You are of legal age (===)');
+// if (ages == 18) console.log('You are of legal age (== )');
+
+
+// const inputAge = prompt('Wat is youe age');
+// console.log(inputAge);
+
+// if ( Number(inputAge) === 20) console.log('Ypu are same age with me');
+
+// const inputAge = prompt('Wat is youe age');
+// console.log(inputAge);
+
+// if (inputAge == 20) console.log('Ypu are same age with me');
+
+// const inputAge = prompt('Wat is youe age');
+// console.log(inputAge);
+
+// if ( inputAge === 20) console.log('Ypu are same age with me');
+
+
+// const inputAge = Number(prompt('Waht is your age'));
+// console.log(inputAge);
+
+// if (inputAge ==== 20) console.log('you are same age me');
+// else if (inputAge ==== 8) console.log('You are the same age of my son');
+// else console.log('You are not the same age of non of us');
+
+
+// 15. Booleans 
+// AND (&&)  -  OR(||)   -   NOT(!)
+
+// 16. Logical Operatoprs
+
+// const hasMotorcycle = true;
+// const hasCar = true;
+
+// console.log( hasMotorcycle && hasCar);
+// console.log(hasMotorcycle || hasCar);
+
+// const hasMotorcycle1 = true;
+// const hasCar1 = false
+
+// console.log( hasMotorcycle1 && hasCar1);
+// console.log(hasMotorcycle1 || hasCar1);
+
+// const hasMotorcycle = true;
+// const hasCar = true;
+
+// console.log( hasMotorcycle && hasCar);
+// console.log(hasMotorcycle || hasCar);
+
+// const isRainy = false;
+// const shuldDruive = hasCar && isRainy;
+
+// if (shuldDruive) {
+//    console.log('Jan shuld drive because rainy'); 
+// } else if (hasMotorcycle && !isRainy) {
+//     console.log('Jan can ride motorcycle');
+// }
+
+
+// 17. switch Statment 
+//  alternative way of writiong comletly writing if staetment 
+// compear one value to multipale defrent options 
+
+
+
+// const number1 = Number(prompt ('Enter your first number'));
+// const operator = prompt ('Enter Operator');
+// const number2 = Number (prompt ('Enter the second number'));
+
+// switch (operator) {
+//    case '+':
+//       console.log('additon');
+//       console.log(`${number1 + number2}`);
+//       break;
+//     case '-':
+//       console.log('substration');
+//       console.log(`${number1 - number2}`);
+//          break;
+//       case '*':
+//       console.log('multiply');
+//       console.log(`${number1 * number2}`);
+//       break;
+//     case '/':
+//       console.log('divide');
+//       console.log(`${number1 / number2}`);
+//       break;
+//    default:
+//       console.log('Invaild operator');
+//       break;
+// }
+
+
+
+// 18. Statements & Expressions
+// EXPRESSION: is a peace of code that produse a value 
+// 8 + 7 
+// 10000
+// true && false && !false
+
+// // STATMENT: like a beger pease of code that executed and whice dose not preduse a value it self 
+// //  EX: if else statmnet 
+// if ( 12 > 10){ 
+//    console.log('12 is biger than 10');
+// }
+
+
+// 19. Ternary Operators 
+
+// const age = 10;
+ 
+// age >= 18;
+// ? console.log('You can drive car')
+// : console.log('You can not drive a car');
+
+// const vehical = age >= 18 ? `car` : `bicycle`;
+// console.log(vehical);
+  
